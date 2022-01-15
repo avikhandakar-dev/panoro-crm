f5995687ac25a4662ef5f47f9d1ce399fc80a69d
Avi Khandakar <avi@Avis-MacBook-Air.local>
2022-01-15 21:57:20 +0600

    add module page

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex justify-center items-center">
+    <div className="h-screen w-full flex justify-center items-center gap-4">
       <Link href="/login">
         <a className="w-32 font-semibold aspect-square flex justify-center items-center bg-primary-500 rounded-md shadow-md text-white">
           Login
+        </a>
+      </Link>
+      <Link href="/module">
+        <a className="w-32 font-semibold aspect-square flex justify-center items-center bg-secondary-500 rounded-md shadow-md text-black">
+          Module
         </a>
       </Link>
     </div>
